88665fcd779466ef0b3543c125c9e079188f8f31
JSiapo <jsiapo@unitru.edu.pe>
2019-10-02 00:24:32 -0500

    Update message

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <DApplication>
 #include <DMainWindow>
 #include <DWidgetUtil>
+#include <QString>
 
 DWIDGET_USE_NAMESPACE
 
@@ -11,15 +12,27 @@ int main(int argc, char *argv[])
     a.setAttribute(Qt::AA_UseHighDpiPixmaps);
 //    a.setTheme("light");
     a.setTheme("dark");
+<<<<<<< HEAD
     a.setOrganizationName("deepinenespañol");
     a.setApplicationName("DTK-Sensor");
     a.setApplicationVersion("1.0");
     a.setProductIcon(QIcon(":/images/dtk-sensor.svg"));
     a.setProductName("DTK-Sensor");
     a.setApplicationDescription("This is a dtk application to see CPU temperature.");
+=======
+    a.setOrganizationName("deepin latin code - Deepin en Español");
+    a.setApplicationName("DTK-Sensor");
+    a.setApplicationVersion("1.0");
+    a.setProductIcon(QIcon(":/images/logo.svg"));
+    a.setProductName("Dtk Application");
+    QString about="<span style=' font-size:8pt; font-weight:600;'>Deepin en Español </span>"
+                  "<a href='https://deepinenespañol.org'>https://deepinenespañol.org</a><br/>"
+                  "<span style=' font-size:8pt; font-weight:600;'>Deepin Latin Code - developers</span>";
+    a.setApplicationDescription(about);
+>>>>>>> Update message
 
     DMainWindow w;
-    w.setMinimumSize(500, 500);
+    w.setMinimumSize(600, 500);
     w.show();
 
     Dtk::Widget::moveToCenter(&w);
